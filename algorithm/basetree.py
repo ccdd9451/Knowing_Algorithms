2debd286d5228fb6de04fdb4c06d5f6458cd0e5d
Kaidong Hu <dongforregister@gmail.com>
2017-05-09 11:55:47 -0400

    basetree and part of BST codes

---
+++ algorithm/basetree.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+
+class BaseTree(object):
+    '''
+    A Tree class that can be used by any algorithm based on it.
+    '''
+    
+    def __init__(self):
+        self.root = None
+        self.size = 0
+
+    def length(self):
+        return self.size
+
+    def __len__(self):
+        return self.size
+
+    def __iter__(self):
+        return self.root.__iter__()
+
+    def __getitem__(self,key):
+        return self.get(key)
+    
+    def __contains__(self,key):
+        if self._get(key,self.root):
+            return True
+        else:
+            return False
+        
+    def __delitem__(self,key):
+        self.delete(key)
+
+
+
+class BaseTreeNode(object):
+    '''
+    Tree node class.
+    '''
+    def __init__(self,key,val,left=None,right=None,
+                                       parent=None):
+        self.key = key
+        self.payload = val
+        self.leftChild = left
+        self.rightChild = right
+        self.parent = parent
+       
+    def __iter__(self):
+        return self.iter()
+
+    def hasLeftChild(self):
+        return self.leftChild
+
+    def hasRightChild(self):
+        return self.rightChild
+
+    def isLeftChild(self):
+        return self.parent and self.parent.leftChild == self
+
+    def isRightChild(self):
+        return self.parent and self.parent.rightChild == self
+
+    def isRoot(self):
+        return not self.parent
+
+    def isLeaf(self):
+        return not (self.rightChild or self.leftChild)
+
+    def hasAnyChildren(self):
+        return self.rightChild or self.leftChild
+
+    def hasBothChildren(self):
+        return self.rightChild and self.leftChild
+
+    def replaceNodeData(self,key,value,lc,rc):
+        self.key = key
+        self.payload = value
+        self.leftChild = lc
+        self.rightChild = rc
+        if self.hasLeftChild():
+            self.leftChild.parent = self
+        if self.hasRightChild():
+            self.rightChild.parent = self
+
+
+
+
+
